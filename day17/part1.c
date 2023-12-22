5fc969feb7272169c665f36847ad555701e57b4e
ggp <>
2023-12-22 10:39:56 +0300

    Minor cleanup of day 17

--- day17/part1.c
+++ day17/part1.c
@@ -1,69 +1,42 @@
 #include <stdio.h>
 
-#include "input_test.h"
+#include "input.h"
 
 #define LEFT  0
 #define RIGHT 1
 #define UP    2
 #define DOWN  3
 
-int mins[4][3][SIZEX][SIZEY];
+#define NDIRECTIONS 4
+#define NSTATES     3
+
+int mins[NDIRECTIONS][NSTATES][SIZEX][SIZEY];
 
 #define MAX -1U>>1
 
-void setupmins()
+void setupmins(void)
 {
 	int i, j, k, l;
-	for (i = 0; i < 4; i++)
-	for (j = 0; j < 3; j++)
+	for (i = 0; i < NDIRECTIONS; i++)
+	for (j = 0; j < NSTATES; j++)
 	for (k = 0; k < SIZEX; k++)
 	for (l = 0; l < SIZEY; l++)
 		mins[i][j][k][l] = MAX;
 }
 
-int mincost()
+int mincost(void)
 {
 	int i, j, min = MAX;
-	for (i = 0; i < 4; i++)
-	for (j = 0; j < 3; j++)
+	for (i = 0; i < NDIRECTIONS; i++)
+	for (j = 0; j < NSTATES; j++)
 		if (mins[i][j][SIZEX-1][SIZEY-1] < min)
 			min = mins[i][j][SIZEX-1][SIZEY-1];
 	return min;
 }
 
-typedef struct {
-	int x;
-	int y;
-	int d;
-	int loss;
-	int step;
-} Path;
-
-void setpath(Path *p, int x, int y, int d, int loss, int step)
+/* dfs is significantly faster than bfs without hashing */
+void dfsexplore(int x, int y, int d, int loss, int step)
 {
-	p->x = x;
-	p->y = y;
-	p->d = d;
-	p->loss = loss;
-	p->step = step;
-}
-
-typedef struct {
-	int  count;
-	Path paths[SIZEX*SIZEY*3*4];
-} Pathset;
-
-Pathset first, second;
-
-void reset(Pathset *ps)
-{
-	ps->count = 0;
-}
-
-void addpath(Pathset *ps, int x, int y, int d, int loss, int step)
-{
-	int i;
-	Path *p;
 	if (x<0 || x>=SIZEX || y<0 || y>=SIZEY)
 		return;
 	loss += map[x][y];
@@ -72,51 +45,28 @@ void addpath(Pathset *ps, int x, int y, int d, int loss, int step)
 	mins[d][step][x][y] = loss;
 	if (loss > 9*(SIZEX + SIZEY) || loss >= mincost())
 		return;
-	for (i = 0, p = ps->paths; i < ps->count; p++, i++) {
-		if (p->x==x && p->y==y && p->d==d && p->step==step) {
-			setpath(p, x, y, d, loss, step);
-			return;
-		}
-	}
-	setpath(p, x, y, d, loss, step);
-	ps->count += 1;
-}
-
-void addnexts(Pathset *ps, Path *p)
-{
-	if (p->d == LEFT || p->d == RIGHT) {
-		addpath(ps, p->x - 1, p->y, UP,   p->loss, 2);
-		addpath(ps, p->x + 1, p->y, DOWN, p->loss, 2);
-		if (p->d == LEFT && p->step)
-			addpath(ps, p->x, p->y - 1, LEFT, p->loss, p->step - 1);
-		if (p->d == RIGHT && p->step)
-			addpath(ps, p->x, p->y + 1, RIGHT, p->loss, p->step - 1);
+	if (d == LEFT || d == RIGHT) {
+		dfsexplore(x - 1, y, UP,   loss, 2);
+		dfsexplore(x + 1, y, DOWN, loss, 2);
+		if (d == LEFT && step)
+			dfsexplore(x, y - 1, LEFT, loss, step - 1);
+		if (d == RIGHT && step)
+			dfsexplore(x, y + 1, RIGHT, loss, step - 1);
 	} else {
-		addpath(ps, p->x, p->y - 1, LEFT,  p->loss, 2);
-		addpath(ps, p->x, p->y + 1, RIGHT, p->loss, 2);
-		if (p->d == UP && p->step)
-			addpath(ps, p->x - 1, p->y, UP, p->loss, p->step - 1);
-		if (p->d == DOWN && p->step)
-			addpath(ps, p->x + 1, p->y, DOWN, p->loss, p->step - 1);
+		dfsexplore(x, y - 1, LEFT,  loss, 2);
+		dfsexplore(x, y + 1, RIGHT, loss, 2);
+		if (d == UP && step)
+			dfsexplore(x - 1, y, UP, loss, step - 1);
+		if (d == DOWN && step)
+			dfsexplore(x + 1, y, DOWN, loss, step - 1);
 	}
 }
-
-#define swap(x, y, type) { type tmp = x; x = y; y = tmp; }
 
 int main(void)
 {
-	Pathset *curr = &first, *next = &second;
 	setupmins();
-	addpath(curr, 0, 1, RIGHT, 0, 2);
-	addpath(curr, 1, 0, DOWN, 0, 2);
-	while (curr->count) {
-		int i;
-		reset(next);
-		for (i = 0; i < curr->count; i++) {
-			addnexts(next, &curr->paths[i]);
-		}
-		swap(curr, next, Pathset*);
-	}
+	dfsexplore(0, 1, RIGHT, 0, 2);
+	dfsexplore(1, 0, DOWN, 0, 2);
 	printf("%d\n", mincost());
 	return 0;
 }
